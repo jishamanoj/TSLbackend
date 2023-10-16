babdeca536b6e27bc9faaf25e1baf4f06ecb1c8b
jishaps3@gmail.com <jishaps3@gmail.com>
2023-10-16 17:16:44 +0530

    set sms api

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const reg = require('../model/registration');
 const router = express.Router();
+const twilio = require('twilio');
 
 router.post('/registerUser', async (req, res) => {
     const { first_name, last_name, DOB, email, country, phone, reference, language,remark } = req.body;
@@ -27,6 +28,32 @@ router.post('/registerUser', async (req, res) => {
         console.error(error);
         res.status(500).send({ message: "An error occurred" });
     }
+});
+const accountSid = 'AC103d96242eb9ce2785095820ec24b563';
+const authToken = '979855b87ed0cfab232674ff02f6795f';
+const twilioPhoneNumber = '+12295979290';
+
+const client = new twilio(accountSid, authToken);
+
+
+router.post('/send-sms', (req, res) => {
+    const to = req.body.to; // Extract 'to' from the request body
+
+    // Use the Twilio client to send an SMS
+    client.messages
+        .create({
+            body: "Your registration is complete! . For the zoom session please send a hi to the WhatsApp number :+91 9008290027",
+            from: twilioPhoneNumber,
+            to: to,
+        })
+        .then((message) => {
+            console.log(`SMS sent: ${message.sid}`);
+            res.status(200).json({ status: 'SMS sent successfully' });
+        })
+        .catch((error) => {
+            console.error('Error sending SMS:', error);
+            res.status(500).json({ error: 'Failed to send SMS' });
+        });
 });
 
 module.exports = router;
